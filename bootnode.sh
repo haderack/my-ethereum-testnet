85dac4b804c9dd57a4353c6da632d4004b582266
Miguel Torres <mtorres@tibco.com>
2016-08-16 11:59:49 -0700

    Add boot node support

---
+++ bootnode.sh
@@ -0,0 +1,1 @@
+../go-ethereum/src/github.com/migueltorreslopez/go-ethereum/build/bin/bootnode --genkey=boot.key && ../go-ethereum/src/github.com/migueltorreslopez/go-ethereum/build/bin/bootnode --nodekey=boot.key
